d63a0784a01c089f8951108479d8bbf737900a9b
siroiZ <144640248+siroiZ@users.noreply.github.com>
2024-04-10 15:06:21 +0900

    a

---
+++ Src/Scene/Title/title.cpp
@@ -0,0 +1,23 @@
+#include"title.h"
+
+
+
+void InitTitle()
+{
+
+}
+
+void StepTitle()
+{
+
+}
+
+void DrawTitle()
+{
+
+}
+
+void FinTitle()
+{
+
+}
---
+++ Src/Scene/Title/title.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef _SCENE_TITLE_H_
+#define _SCENE_TITLE_H_
+
+void InitTitle();
+
+void StepTitle();
+
+void DrawTitle();
+
+void FinTitle();
+
+#endif
