ace5d4ce327b4a8d3b02539a0c5700282205fbf6
Maruru3n <fukushimaks121@icloud.com>
2024-04-11 09:51:28 +0900

    お

--- Src/Player/player.h
+++ Src/Player/player.h
@@ -17,10 +17,31 @@ protected:
 
 public:
 
+	//初期化
 	void Init();
 
+	//通常処理
 	void Step();
 
+	//描画処理
 	void Draw();
-	
+
+
+	//プレイヤーのｘ座標を得る
+	float GetPlayerPosX() { return playerX; }
+
+	//プレイヤーのｙ座標を得る
+	float GetPlayerPosY() { return playerY; }
+
+	//プレイヤーのx座標をセット
+	float GetPlayerPosX(float new_pos_x) {  playerX= new_pos_x; }
+
+	//プレイヤーのｙ座標をセット
+	float GetPlayerPosY(float new_pos_y) {  playerY= new_pos_y; }
+
+	//プレイヤーのＸサイズ
+	int GetPlayerSizeX() { return playerSizeX; }
+
+	//プレイヤーのＹサイズ
+	int GetPlayerSizeY() { return playerSizeY; }
 };
